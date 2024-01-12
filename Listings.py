95add3c73d23fc7a4b15eaaaf8b380ca4874c5fa
emil <emil.deshayes.8462@student.uu.se>
2024-01-12 23:47:14 +0900

    first commit, added scraper

---
+++ Listings.py
@@ -0,0 +1,6 @@
+class Listings:
+    def __init__(self, house_type, rooms, size, price):
+        self.house_type = house_type
+        self.rooms = rooms
+        self.size = size
+        self.price = price
